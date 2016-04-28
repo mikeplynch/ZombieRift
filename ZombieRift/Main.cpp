d3ebd296b8440495b937bf10e7a89dba92b531f7
Aidan McInerny <aidan@mcinerny.org>
2016-04-28 19:18:30 -0400

    Now supports OpenGL 3.1!

--- ZombieRift/Main.cpp
+++ ZombieRift/Main.cpp
@@ -20,11 +20,19 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	int width = 1024;
+	int height = 768;
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow(1024, 768, "MyMesh class", NULL, NULL);
+	window = glfwCreateWindow(width, height, "MyMesh class", NULL, NULL);
 	if (window == NULL){
-		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		fprintf(stderr, "Failed to open GLFW window with OpenGL 3.3.  Attempting 3.1\n");
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+		window = glfwCreateWindow(width, height, "MyMesh class", NULL, NULL);
+		if (window == NULL)
+		{
+			fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		}
 		glfwTerminate();
 		return -1;
 	}
