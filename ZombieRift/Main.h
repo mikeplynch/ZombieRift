8b3c3bb71d0a83a1819001938289e66af8e909da
Aidan McInerny <aidan@mcinerny.org>
2016-04-30 14:48:03 -0400

    Adding Kinect Setup, install the SDK: https://www.microsoft.com/en-us/download/details.aspx?id=44561

--- ZombieRift/Main.h
+++ ZombieRift/Main.h
@@ -5,6 +5,13 @@ This project was generated in 2013
 #define _MAIN_H
 
 #include <iostream>
+
+//Download the kinect 2.0 SDK: https://www.microsoft.com/en-us/download/details.aspx?id=44561
+#include <Windows.h>
+#include <Ole2.h>
+#include <Kinect.h>
+
+
 #include "Camera.h"
 #include "SimpleObjects.h"
 #include "GameManager.h"
